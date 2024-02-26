bcdde798bb184a39e70d0deecfd1f9d2e92f2ecd
cecilgachie <159792219+cecilgachie@users.noreply.github.com>
2024-02-26 17:06:32 +0300

    Add files via upload
    php connection

---
+++ connection2.php
@@ -0,0 +1,20 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "vatican";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+// Perform SQL queries or operations here
+
+?>
